e90f2781db931079872aa601d400891a446841b6
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-21 22:45:52 -0500

    added basic outline for code w/ link, lists, and list items all included for now

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,8 +1,30 @@
+import { Link } from 'react-router-dom';
+
 export default function ResumePage() {
 
     return (
       <div className="">
-          <h2>Currently under construction. Come back soon!</h2>
+          <h3>Resume</h3>
+          <Link to={`#`} className="badge bg-primary rounded-pill">
+              Download my resume
+            </Link>
+        <ul className="">
+            <h5>Front-end Proficiencies</h5>
+            <li className="">HTML</li>
+            <li className="">CSS</li>
+            <li className="">JavScript</li>
+            <li className="">React</li>
+            <li className="">Bootstrap</li>
+            <li className="">Responsive Design</li>
+        </ul>
+        <ul className="">
+            <h5>Back-end Proficiencies</h5>
+            <li className="">APIs</li>
+            <li className="">Node</li>
+            <li className="">Express</li>
+            <li className="">SQL</li>
+        </ul>
       </div>
+      
     );
   }
