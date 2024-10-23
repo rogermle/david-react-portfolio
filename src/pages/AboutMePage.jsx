cf48417607d3b0b5af4ab04a2f1af850942684df
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-23 00:04:12 -0500

    added my photo and adjusted styling

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,11 +1,15 @@
 // This is a static page mocking an "About Us" section for our fake user data
 export default function AboutMePage() {
-    return (
-      <div className="">
-         <h3>About Me</h3>
-         {/* add photo here */}
-         <img src="../../../../src/assets/img/david.png" class="card-img" alt="..." />
-        <p>
+  return (
+    <div className="mx-4">
+      <h3 className="">About Me</h3>
+      {/* add photo here */}
+      <div className="row">
+        <div className="col">
+        <img src="../../../../src/assets/img/david.png" className="mx-auto p-5 img-fluid" alt="..." />
+        </div>
+        <div className="col">
+        <p className="">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
           tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
           impedit suscipit sed magnam alias in, repellat expedita hic explicabo
@@ -14,7 +18,9 @@ export default function AboutMePage() {
           mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
           nostrum temporibus ad omnis nam rerum eligendi.
         </p>
+        </div>
+        {/* <div className="col"></div> */}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
