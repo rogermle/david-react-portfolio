18257a8a58a9fe0f032ee865d7c92323c1088892
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-22 22:06:07 -0500

    changed resume download link to anchor tag

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -2,29 +2,30 @@ import { Link } from 'react-router-dom';
 
 export default function ResumePage() {
 
-    return (
-      <div className="">
-          <h3>Resume</h3>
-          <Link to={`#`} className="badge bg-primary rounded-pill">
-              Download my resume
-            </Link>
-        <ul className="">
-            <h5>Front-end Proficiencies</h5>
-            <li className="">HTML</li>
-            <li className="">CSS</li>
-            <li className="">JavScript</li>
-            <li className="">React</li>
-            <li className="">Bootstrap</li>
-            <li className="">Responsive Design</li>
-        </ul>
-        <ul className="">
-            <h5>Back-end Proficiencies</h5>
-            <li className="">APIs</li>
-            <li className="">Node</li>
-            <li className="">Express</li>
-            <li className="">SQL</li>
-        </ul>
-      </div>
-      
-    );
-  }
+  return (
+    <div className="">
+      <h3>Resume</h3>
+      {/* <Link to={`../../../../src/assets/david-cartolano-resume.pdf`} className="badge bg-primary rounded-pill">
+        Download my resume
+      </Link> */}
+      <a href="../../../../src/assets/david-cartolano-resume.pdf" download>Download my resume</a>
+      <ul className="">
+        <h5>Front-end Proficiencies</h5>
+        <li className="">HTML</li>
+        <li className="">CSS</li>
+        <li className="">JavScript</li>
+        <li className="">React</li>
+        <li className="">Bootstrap</li>
+        <li className="">Responsive Design</li>
+      </ul>
+      <ul className="">
+        <h5>Back-end Proficiencies</h5>
+        <li className="">APIs</li>
+        <li className="">Node</li>
+        <li className="">Express</li>
+        <li className="">SQL</li>
+      </ul>
+    </div>
+
+  );
+}
