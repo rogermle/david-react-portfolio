abe7ca2a7269ea87af0b0d41b255a2005aeef9b5
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-22 21:44:32 -0500

    header now formats full width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navigation from "./Navigation";
 
 function Header() {
     return (
-        <header>
+        <header class="d-flex flex-wrap justify-content-between py-3 mb-4 border-bottom bg-dark">
             <h1>Header</h1>
             <Navigation />
         </header>
