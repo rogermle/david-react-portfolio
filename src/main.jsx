eb84ed922b8c291247a26187ce66d67ae5d0b111
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-22 21:43:17 -0500

    added comment about bootstrap import

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // bootstrap
 import './index.css';
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
