ea317ef7248d92f76ce040f49c66df8dd6a84561
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-23 00:03:46 -0500

    removed -lg for navbar-expand class so navbar would show up on all screen sizes

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,7 +1,7 @@
 // COPIED FROM 12-24 AND EDITED
 export default function Nav({ links }) {
     return (
-      <nav className="navbar navbar-expand-lg ">
+      <nav className="navbar navbar-expand ">
         <div className="container-fluid">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
